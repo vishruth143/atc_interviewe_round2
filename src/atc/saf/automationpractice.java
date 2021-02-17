83361f8b6698d155bc34e366336d1ca0b32551e0
vishr <vishr@LAPTOP-9MKQQPJI>
2021-02-17 21:36:46 +0530

    Refined Code

--- src/atc/saf/automationpractice.java
+++ src/atc/saf/automationpractice.java
@@ -1,21 +1,26 @@
 package atc.saf;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
 
 
 public class automationpractice {
 	
 	public static WebDriver driver = null;
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
@@ -80,7 +85,74 @@ public class automationpractice {
 		Thread.sleep(20000);
 		
 		driver.findElement(By.xpath("//a[text()='Summer Dresses']")).click();	
+		
+		// 6. The Items would be in 'Grid view'. Change it to 'List View'.
+		driver.findElement(By.xpath("//li[@id='list']/a/i")).click();	
+		
+		
+		// 7. Click on the first item to view.
+		driver.findElement(By.xpath("(//img[@alt='Printed Summer Dress'])[2]")).click();			
+		// 8. Increase the quantity to 5
+		for (int i=0; i<4; i++) {
+			driver.findElement(By.xpath("//p[@id='quantity_wanted_p']/a[2]/span/i")).click();
 			
+		}		
+		// 9. Change the size to 'L'
+		Select size = new Select(driver.findElement(By.id("group_1")));
+		size.selectByVisibleText("L");		
+		// 10. Select any colour
+		driver.findElement(By.id("color_11")).click();		
+		// 11. Add to cart
+		driver.findElement(By.id("//span[text()='Add to cart']")).click();		
+		
+		
+		// 12. Click 'Continue shopping' and repeat the same for the other 2 items as well under the summer dresses.
+		driver.findElement(By.xpath("//div[@id='layer_cart']/div/div[2]/div[4]/span/span")).click();
+		driver.navigate().back();
+		driver.findElement(By.xpath("(//img[@alt='Printed Summer Dress'])[3]")).click();		
+		for (int i=0; i<4; i++) {
+			driver.findElement(By.xpath("//p[@id='quantity_wanted_p']/a[2]/span/i")).click();
+		}		
+		size.selectByVisibleText("L");		
+		driver.findElement(By.id("color_8")).click();		
+		driver.findElement(By.id("//span[text()='Add to cart']")).click();	
+		
+		
+		driver.findElement(By.xpath("//div[@id='layer_cart']/div/div[2]/div[4]/span/span")).click();
+		driver.navigate().back();
+		driver.findElement(By.xpath("//img[@alt='Printed Chiffon Dress']")).click();		
+		for (int i=0; i<4; i++) {
+			driver.findElement(By.xpath("//p[@id='quantity_wanted_p']/a[2]/span/i")).click();			
+		}		
+		size.selectByVisibleText("L");		
+		driver.findElement(By.id("color_15")).click();		
+		driver.findElement(By.id("//span[text()='Add to cart']")).click();	
+		
+		// 13. Proceed to checkout and complete the payment
+		driver.findElement(By.xpath("//div[@id='layer_cart']/div/div[2]/div[4]/a/span")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Proceed to checkout']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Proceed to checkout']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("cgv")).click();		
+		driver.findElement(By.xpath("//form[@id='form']/p/button/span")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("\"Pay by bank wire (order processing will be longer)\"")).click();
+		
+		driver.findElement(By.linkText("//span[text()='I confirm my order']")).click();
+		
+		// 14. Move to your profile and check 'order history and details'
+		driver.findElement(By.xpath("//header[@id='header']/div[2]/div/div/nav/div/a/span")).click();
+		driver.findElement(By.xpath("//span[text()='Order history and details']")).click();			
+		
+		// 15. Capture screenshot of the order history
+		File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshotFile , new File(".\\screenshot.png"));
+		
+		// 16. Sign out from the application
+		driver.findElement(By.linkText("Sign out")).click();
+					
 		// Close the web driver		
 		driver.close();
 		driver.quit();
